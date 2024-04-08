4abebbc2096ab1598e0a87868a8853337c06766b
benjamin Brookarsh <BenB@ideo-digital.com>
2024-04-08 15:32:24 +0300

    remove unused code

--- Client/src/app/interceptors/http-interceptor.ts
+++ Client/src/app/interceptors/http-interceptor.ts
@@ -1,22 +1,5 @@
-import {Injectable} from '@angular/core';
-import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpInterceptorFn} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpInterceptorFn} from '@angular/common/http';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + token)
-      });
-      return next.handle(cloned);
-    } else {
-      return next.handle(req);
-    }
-  }
-}
 
 export const TokenInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('TOKEN') ?? '';
